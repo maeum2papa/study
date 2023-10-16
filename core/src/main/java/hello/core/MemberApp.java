7304fc5b1968978c6db6f9c03a6dd2454e4779f7
Kim Jae-Won <okhi1@naver.com>
2023-10-16 12:05:23 +0900

    4일차
    spring 흉내내보기

--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -1,2 +1,21 @@
-package hello.core;public class MemberApp {
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+
+public class MemberApp {
+
+    public static void main(String[] args) {
+
+        AppConfing appConfing = new AppConfing();
+        MemberService memberService = appConfing.memberService();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findByMember(1L);
+        System.out.println("member = " + member.getName());
+        System.out.println("findMember = " + findMember.getName());
+    }
 }
--- core/src/test/java/hello/core/member/MemberServiceTest.java
+++ core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,2 +1,32 @@
-package hello.core.member;public class MemberServiceTest {
+package hello.core.member;
+
+import hello.core.AppConfing;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach(){
+        AppConfing appConfing = new AppConfing();
+        this.memberService = appConfing.memberService();
+    }
+
+    @Test
+    void join(){
+        //given
+        Member member = new Member(1L,"memberA",Grade.VIP);
+
+
+        //when
+        memberService.join(member);
+        Member findMember = memberService.findByMember(1L);
+
+
+        //then
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
 }

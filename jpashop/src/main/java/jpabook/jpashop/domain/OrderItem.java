12c18167b92720891609aa51bda22b493433dd36
Kim Jae-Won <okhi1@naver.com>
2023-10-25 17:01:59 +0900

    API 개발 및 성능 최적화

--- jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jpabook.jpashop.domain.item.Item;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -24,6 +25,7 @@ public class OrderItem {
     @JoinColumn(name="item_id")
     private Item item;
 
+    @JsonIgnore
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "order_id")
     private Order order;
@@ -40,6 +42,8 @@ public class OrderItem {
         item.removeStock(count);
         return orderItem;
     }
+
+
 
     public void cancel() {
         getItem().addStock(count);
